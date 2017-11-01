e663d270561a67b7f6b4917656965d58b41bb1c4
Robert G. Raleigh <robert@helloworlddevs.com>
2017-11-01 13:34:51 -0700

    webpack config, base index.html template

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+/*jshint esversion: 6 */
+// app.js
+
+// import main scss file
+import '../scss/app.scss';
+// import font-awesome-webpack
+import 'font-awesome-webpack';
+// import foundation sass
+// import 'foundation-sites/scss/foundation.scss';
+
+// import foundation-sites js
+import { Foundation } from 'foundation-sites/js/foundation.core.js';
---
+++ webpack.config.js
@@ -0,0 +1,93 @@
+/*jshint esversion: 6 */
+(function () {
+  "use strict";
+  const path = require("path");
+  const webpack = require("webpack");
+  const ExtractTextWebpackPlugin = require("extract-text-webpack-plugin");
+  const CopyWebpackPlugin = require("copy-webpack-plugin");
+  const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+  const config = {
+    entry: {
+      app: './src/js/app.js'
+    },
+    output: {
+      filename: '[name].build.js',
+      path: path.resolve(__dirname + '/build')
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['env']
+            }
+          }
+        },
+        {
+          test: /\.css$/,
+          use: 'css-loader'
+        },
+        {
+          test: /\.(scss|sass)$/,
+          use: ExtractTextWebpackPlugin.extract({
+            fallback: "style-loader",
+            use: ["css-loader", "sass-loader"],
+          })
+        },
+        { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          loader: "url-loader?limit=10000&mimetype=application/font-woff",
+          options: {
+            name: "fonts/[name].[ext]"
+          }
+        },
+        { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          loader: "file-loader",
+          options: {
+            name: "fonts/[name].[ext]"
+          }
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif)/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: "img/[name].[ext]",
+              }
+            }
+          ]
+        }
+      ]
+    },
+    watch: true,
+    plugins: [
+      new ExtractTextWebpackPlugin("css/app.build.css"),
+      new HtmlWebpackPlugin({
+        title: 'Index`',
+        filename: 'index.html',
+        template: 'src/index.html'
+      }),
+      new HtmlWebpackPlugin({
+        title: 'About',
+        filename: 'about.html',
+        template: "src/about.html"
+      }),
+      new CopyWebpackPlugin([
+        {
+          from: './src/img',
+          to: 'img'
+        },
+        {
+          from: './src/media',
+          to: 'media'
+        }
+      ])
+    ]
+  };
+
+  module.exports = config;
+}());
